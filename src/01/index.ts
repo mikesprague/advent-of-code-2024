408d370535e0f31be5c39eb51c0147276aea21eb
Michael Sprague <mikesprague@gmail.com>
2024-12-02 17:24:09 -0500

    feat(01): add sample code to read input file

--- src/01/index.ts
+++ src/01/index.ts
@@ -1,11 +1,20 @@
 // https://adventofcode.com/2024/day/1
 
+import { getFileAsIterator } from '../../lib/helpers.ts';
+
 const { hrtime } = process;
+
+const inputFile = './src/01/input.txt';
 
 (async () => {
   const debugStart = hrtime();
 
-  // add code here
+  // console.log(inputFile);
+
+  const file = await getFileAsIterator(inputFile);
+  for await (const line of file.readLines()) {
+    console.log(line);
+  }
 
   const debugEnd = hrtime(debugStart);
   console.log(
