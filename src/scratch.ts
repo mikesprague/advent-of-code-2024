f42ef27616d0e09064bfc10e4ae8418a71305942
Michael Sprague <mikesprague@gmail.com>
2024-12-02 15:16:40 -0500

    init: start project
    Signed-off-by: Michael Sprague <mikesprague@gmail.com>

---
+++ src/scratch.ts
@@ -0,0 +1,12 @@
+const { hrtime } = process;
+
+(async () => {
+  const debugStart = hrtime();
+
+  // add code here
+
+  const debugEnd = hrtime(debugStart);
+  console.log(
+    `Execution time: ${debugEnd[0] * 1000 + debugEnd[1] / 1000000}ms`
+  );
+})();
