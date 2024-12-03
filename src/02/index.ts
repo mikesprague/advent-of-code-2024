a4592c3b6689f2b9ecf2f127a848f05e9a44fb6e
Michael Sprague <mikesprague@gmail.com>
2024-12-02 20:51:31 -0500

    refactor(02): use checkReport helper

--- src/02/index.ts
+++ src/02/index.ts
@@ -1,6 +1,6 @@
 // https://adventofcode.com/2024/day/2
 
-import { getFileAsIterator } from '../../lib/helpers.ts';
+import { checkReport, getFileAsIterator } from '../../lib/helpers.ts';
 
 const { hrtime } = process;
 
@@ -14,43 +14,6 @@ const { hrtime } = process;
   const file = await getFileAsIterator(inputFile);
 
   const safeReports = [];
-
-  const checkReport = (report: string[]) => {
-    let increasing = false;
-    let decreasing = false;
-
-    for (let i = 0; i < report.length - 1; i++) {
-      // get the current and next numbers in the report
-      const current = Number(report[i].trim());
-      const next = Number(report[i + 1].trim());
-
-      // determine if the report is increasing or decreasing
-      if (current < next) {
-        increasing = true;
-      } else if (current > next) {
-        decreasing = true;
-      }
-
-      if (current === next) {
-        return false;
-      }
-
-      // if diff is more than 3, break (unsafe)
-      if (Math.abs(current - next) > 3) {
-        return false;
-      }
-
-      // if both increasing and decreasing, break (unsafe)
-      if (increasing && decreasing) {
-        return false;
-      }
-
-      // we're at the end of the report, it's safe
-      if (i === report.length - 2) {
-        return true;
-      }
-    }
-  };
 
   // iterate through the file line by line
   for await (const line of file.readLines()) {
