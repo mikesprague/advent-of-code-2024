cf40d5e15050be44bdfe2be7d6bdbf96c0b27fcc
Michael Sprague <mikesprague@gmail.com>
2024-12-02 20:39:49 -0500

    refactor(02): add code for part two

--- src/02/index.ts
+++ src/02/index.ts
@@ -15,48 +15,59 @@ const { hrtime } = process;
 
   const safeReports = [];
 
+  const checkReport = (report: string[]) => {
+    let increasing = false;
+    let decreasing = false;
+
+    for (let i = 0; i < report.length - 1; i++) {
+      // get the current and next numbers in the report
+      const current = Number(report[i].trim());
+      const next = Number(report[i + 1].trim());
+
+      // determine if the report is increasing or decreasing
+      if (current < next) {
+        increasing = true;
+      } else if (current > next) {
+        decreasing = true;
+      }
+
+      if (current === next) {
+        return false;
+      }
+
+      // if diff is more than 3, break (unsafe)
+      if (Math.abs(current - next) > 3) {
+        return false;
+      }
+
+      // if both increasing and decreasing, break (unsafe)
+      if (increasing && decreasing) {
+        return false;
+      }
+
+      // we're at the end of the report, it's safe
+      if (i === report.length - 2) {
+        return true;
+      }
+    }
+  };
+
   // iterate through the file line by line
   for await (const line of file.readLines()) {
     if (line.trim().length) {
       const report = line.split(' ');
       // console.log(report);
 
-      let increasing = false;
-      let decreasing = false;
+      if (checkReport(report)) {
+        safeReports.push(report.join(' '));
+      } else {
+        for (let i = 0; i < report.length; i++) {
+          const nextReport = report.filter((_, index) => index !== i);
 
-      for (let i = 0; i < report.length - 1; i++) {
-        // get the current and next numbers in the report
-        const current = Number(report[i].trim());
-        const next = Number(report[i + 1].trim());
-
-        // determine if the report is increasing or decreasing
-        if (current < next) {
-          increasing = true;
-        } else if (current > next) {
-          decreasing = true;
-        }
-
-        if (current === next) {
-          // console.log('unsafe (no change): ', report.join(' '));
-          break;
-        }
-
-        // if diff is more than 3, break (unsafe)
-        if (Math.abs(current - next) > 3) {
-          // console.log('unsafe (more than 3): ', report.join(' '));
-          break;
-        }
-
-        // if both increasing and decreasing, break (unsafe)
-        if (increasing && decreasing) {
-          // console.log('unsafe (up and down): ', report.join(' '));
-          break;
-        }
-
-        // we're at the end of the report, it's safe
-        if (i === report.length - 2) {
-          // console.log('safe: ', report.join(' '));
-          safeReports.push(report);
+          if (checkReport(nextReport)) {
+            safeReports.push(report.join(' '));
+            break;
+          }
         }
       }
     }
